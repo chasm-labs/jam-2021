351257975bd8b46a25b9672b1799b03a681fb340
Shawn Toubeau <shawntoubeau@gmail.com>
2021-11-04 20:50:17 -0400

    feat(engine): adds basic movement for caterpillar

--- GameController.gd
+++ GameController.gd
@@ -6,27 +6,31 @@ var window_border
 func _ready():
 	window_border = OS.get_real_window_size()
 	var classLoad = load("res://player/caterpillar/caterpillar.gd")
-	caterpillar = classLoad.new()
+	caterpillar = classLoad.new(5)
 
 
 func draw_caterpillar():
-	print("draw")
-	if(caterpillar.body.size() > $caterpillar.get_child_count()):
-		var lastChilde = $sncaterpillarake.get_child($caterpillar.get_child_count()-1).duplicate()
+	if (caterpillar.body.size() > $caterpillar.get_child_count()):
+		var lastChilde = $caterpillar.get_child($caterpillar.get_child_count()-1).duplicate()
 		lastChilde.name = "body " + str($caterpillar.get_child_count())
 		$caterpillar.add_child(lastChilde)
-	for index in range(0,caterpillar.body.size()):
-		$caterpillar.get_child(index).rect_position = caterpillar.body[index]
+	if (caterpillar.direction != Vector2(0,0)):
+		for index in range(0,caterpillar.body.size()):
+			$caterpillar.get_child(index).rect_position = caterpillar.body[index]
 
 func _input(_event):
+	var moveIncrement = caterpillar.width;
+
 	if Input.is_action_pressed("ui_right") and not caterpillar.direction == Vector2(-caterpillar.width,0):
-		caterpillar.direction = Vector2(caterpillar.width,0)
+		caterpillar.direction = Vector2(moveIncrement,0)
 	elif Input.is_action_pressed("ui_left")and not caterpillar.direction == Vector2(caterpillar.width,0):
-		caterpillar.direction = Vector2(-caterpillar.width,0)
+		caterpillar.direction = Vector2(-moveIncrement,0)
 	elif Input.is_action_pressed("ui_down") and not caterpillar.direction == Vector2(0,-caterpillar.width):
-		caterpillar.direction = Vector2(0,caterpillar.width)
+		caterpillar.direction = Vector2(0,moveIncrement)
 	elif Input.is_action_pressed("ui_up")and not caterpillar.direction == Vector2(0,caterpillar.width):
-		caterpillar.direction = Vector2(0,-caterpillar.width)
+		caterpillar.direction = Vector2(0,-moveIncrement)
+	else:
+		caterpillar.direction = Vector2(0,0)
 
 func _on_Timer_timeout():
 	caterpillar.move()
--- player/caterpillar/caterpillar.gd
+++ player/caterpillar/caterpillar.gd
@@ -1,15 +1,22 @@
 class_name caterpillar
-var body
+var body = []
 var direction:Vector2
 const width = 40
 var body_copy
 
-func _init():
-	body = [Vector2(width,0), Vector2(0,0)]
-	direction = Vector2(width, 0)
+func _init(numSegments):
+	for currSegment in range(0, numSegments):
+		body.insert(currSegment, Vector2(width * currSegment, 0))
+	direction = Vector2(0, 0)
 
 func move():
-	body_copy = body.slice(0,body.size() - 2)
-	var new_head = body_copy[0] + direction
-	body_copy.insert(0,new_head)
-	body = body_copy
+    # Update the body's position while we're still moving in a direction
+	if (direction != Vector2(0, 0)):
+		# Retrieve just the body of the caterpillar, -2 to account for the head and regular 0-based array offset
+		body_copy = body.slice(0,body.size() - 2)
+		# Update the head's direction
+		var new_head = body_copy[0] + direction
+		# Insert the new head at the start of the body
+		body_copy.insert(0,new_head)
+		# Update the body of the class
+		body = body_copy
